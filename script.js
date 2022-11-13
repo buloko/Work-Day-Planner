2335f730f62ac1da99e0cb3e4f20452a3d59458f
buloko <buloko8@gmail.com>
2022-11-13 12:17:21 -0500

    commit starter code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const dayJsObject =daysjs();
+console.log(dayJsObject.format('D/M/YYYY'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,10 +18,11 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  //.
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(today.format('MM D, YYYY'));
 });
