f9bc931f384e2c169fc571c18dfa26ef9335b71d
buloko <buloko8@gmail.com>
2023-04-01 03:07:14 -0400

    fixing the IDs and fixing the logic

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-const today = dayjs();
+var today = dayjs();
 $("#currentDay").text(today.format("MMM D, YYYY"));
 
+var saveButtonEl = document.querySelector("save");
+var now = dayjs().hour();
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour13 = $("#13");
+var hour14 = $("#14");
+var hour15 = $("#15");
+var hour16 = $("#16");
+var hour17 = $("#17");
+
 $(document).ready(function () {
+
   $(".saveBtn").on("click", function () {
-    const text = $(this).siblings(".description").val();
-    const time = $(this).parent().attr("id");
+
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
     localStorage.setItem(time, text);
   });
 
-  $("#9 .description").val(localStorage.getItem("9"));
-  $("#10 .description").val(localStorage.getItem("10"));
-  $("#11 .description").val(localStorage.getItem("11"));
-  $("#12 .description").val(localStorage.getItem("12"));
-  $("#13 .description").val(localStorage.getItem("13"));
-  $("#14 .description").val(localStorage.getItem("14"));
-  $("#15 .description").val(localStorage.getItem("15"));
-  $("#16 .description").val(localStorage.getItem("16"));
-  $("#17 .description").val(localStorage.getItem("17"));
-
-  $(".time-block").each(function () {
-    const hour = dayjs().format("H");
-    const divId = parseInt($(this).attr("id"));
-    if (divId < hour) {
-      $(this).removeClass("present future").addClass("past");
-    } else if (divId === hour) {
-      $(this).removeClass("past future").addClass("present");
-    } else {
-      $(this).removeClass("past present").addClass("future");
-    }
-  });
+  $("#9 .description").val(localStorage.getItem("9"))
+  $("#10 .description").val((localStorage.getItem("10")))
+  $("#11 .description").val((localStorage.getItem("11")))
+  $("#12 .description").val((localStorage.getItem("12")))
+  $("#13 .description").val((localStorage.getItem("13")))
+  $("#14 .description").val((localStorage.getItem("14")))
+  $("#15 .description").val((localStorage.getItem("15")))
+  $("#16 .description").val((localStorage.getItem("16")))
+  $("#17 .description").val((localStorage.getItem("17")))
+  
+$(".time-block").each(function(){
+var hour = dayjs().format("H")
+console.log(hour);
+var divId = parseInt($(this).attr("id"))
+if (divId<hour) {
+  $(this).removeClass("present")
+  $(this).removeClass("future")
+  $(this).addClass("past")
+  
+}
+else if (divId == hour) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+}
+else {
+   $(this).removeClass("past");
+   $(this).removeClass("present");
+   $(this).addClass("future");
+}
+}) 
 });
